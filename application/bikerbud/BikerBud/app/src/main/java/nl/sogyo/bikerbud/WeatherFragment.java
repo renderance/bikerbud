2f04346d11ab2e3230bf6206a579fed1361aa56e
gmulder <gmulder@sogyo.nl>
2021-02-03 17:31:38 +0100

    Added formatting to the weather-fragment window, added icons for different weather conditions and made weather-fragment window scrollable in case there are more warnings than fit on the screen.

--- application/bikerbud/BikerBud/app/src/main/java/nl/sogyo/bikerbud/WeatherFragment.java
+++ application/bikerbud/BikerBud/app/src/main/java/nl/sogyo/bikerbud/WeatherFragment.java
@@ -1,64 +1,22 @@
 package nl.sogyo.bikerbud;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
-public class WeatherFragment extends Fragment implements LocationListener {
+public class WeatherFragment extends Fragment {
 
     public WeatherFragment() {
         // Required empty public constructor
     }
 
-    protected LocationManager locationManager;
-    protected LocationListener locationListener;
-    protected Context context;
-    TextView txtLat;
-    String lat;
-    String provider;
-    protected String latitude, longitude;
-    protected boolean gps_enabled, network_enabled;
-
-    @SuppressLint("MissingPermission")
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        txtLat = getActivity().findViewById(R.id.weathertext);
-        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, this);
-    }
-    @Override
-    public void onLocationChanged(Location location) {
-        txtLat = getActivity().findViewById(R.id.weathertext);
-        txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        Log.d("Latitude","status");
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
